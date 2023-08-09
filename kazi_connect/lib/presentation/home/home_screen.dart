903fcd6433b5d175ecb5ac62478e179b14dcc635
samuel-wakoli <swwakoli@gmail.com>
2023-08-09 20:35:06 +0300

    added bottom nav

--- kazi_connect/lib/presentation/home/home_screen.dart
+++ kazi_connect/lib/presentation/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -7,14 +8,69 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
+int currentPageIndex = 0;
+
 class _HomeScreenState extends State<HomeScreen> {
+  List<IconData> bottomBarIconsData = [
+    Icons.home_outlined,
+    Icons.business_center_outlined,
+    Icons.message_outlined,
+    Icons.account_box_outlined
+  ];
+
+  List<String> appbarTitles = [
+    "Kazi Connect",
+    "Jobs",
+    "Messages",
+    "Profile",
+  ];
+
+  List<String> bottomBarIconLabels = [
+    "Home",
+    "Jobs",
+    "Messages",
+    "Profile",
+  ];
+
+  List<Widget> homeScreenPages = const [
+    Column(),
+    Column(),
+    Column(),
+    Column(),
+  ];
+
+  Widget _getBottomBar() => BottomNavigationBar(
+        currentIndex: currentPageIndex,
+        onTap: (index) => setState(() => currentPageIndex = index),
+        backgroundColor:
+            Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+        type: BottomNavigationBarType.fixed,
+        items: List.generate(
+          bottomBarIconsData.length,
+          (index) => BottomNavigationBarItem(
+            icon: Icon(bottomBarIconsData[index]),
+            label: bottomBarIconLabels[index],
+          ),
+          growable: false,
+        ),
+        selectedIconTheme: IconThemeData(
+          color: Theme.of(context).primaryColor,
+        ),
+        selectedItemColor: Theme.of(context).primaryColor,
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text("Home"),
+        title: Text(
+          appbarTitles[currentPageIndex],
+          style: TextStyle(color: Theme.of(context).primaryColor),
+        ),
       ),
+      body: homeScreenPages[currentPageIndex],
+      bottomNavigationBar: _getBottomBar(),
     );
   }
 }
--- kazi_connect/lib/presentation/start/onboarding_page.dart
+++ kazi_connect/lib/presentation/start/onboarding_page.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+import 'package:kazi_connect/presentation/home/home_screen.dart';
+import 'package:kazi_connect/utils.dart';
 
 import 'auth_gate.dart';
 
@@ -45,112 +48,112 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
       pageColor: Colors.white,
       imagePadding: EdgeInsets.zero,
     );
-    return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 0,
-      ),
-      body: IntroductionScreen(
-        key: introKey,
-        globalBackgroundColor: Colors.white,
-        allowImplicitScrolling: true,
-        autoScrollDuration: 4500, // 4.5 seconds
-        infiniteAutoScroll: true,
-        globalHeader: Align(
-          alignment: Alignment.topRight,
-          child: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 16, right: 16),
-              child: _buildImage('logo.png', 100),
+    return StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          // If user is signed in, return HomeScreen
+          if (snapshot.hasData) {
+            return const HomeScreen();
+          }
+          return Scaffold(
+            appBar: AppBar(
+              toolbarHeight: 0,
             ),
-          ),
-        ),
-        globalFooter: SizedBox(
-          width: double.infinity,
-          height: 60,
-          child: ElevatedButton(
-            child: const Text(
-              'Let\'s go right away!',
-              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+            body: IntroductionScreen(
+              key: introKey,
+              globalBackgroundColor: Colors.white,
+              allowImplicitScrolling: true,
+              autoScrollDuration: 6000,
+              // 6 seconds
+              infiniteAutoScroll: true,
+              globalHeader: Align(
+                alignment: Alignment.topRight,
+                child: SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 16, right: 16),
+                    child: _buildImage('logo.png', 100),
+                  ),
+                ),
+              ),
+              pages: [
+                PageViewModel(
+                  title: "Welcome to Kazi Connect",
+                  body:
+                      "Connecting You to short-term, full-time jobs and freelancers in Kenya",
+                  image: _buildImage('welcome_image.jpg'),
+                  decoration: pageDecoration,
+                ),
+                PageViewModel(
+                  title: "Discover Job Opportunities",
+                  body:
+                      "Explore a variety of short-term job listings across different industries and roles.",
+                  image: _buildImage('discover_jobs_image.jpg'),
+                  decoration: pageDecoration,
+                ),
+                PageViewModel(
+                  title: "Easy Application Process",
+                  body:
+                      "Apply for jobs with a few taps, making your job search efficient and hassle-free.",
+                  image: _buildImage('easy_application_image.jpg'),
+                  decoration: pageDecoration,
+                ),
+                PageViewModel(
+                  title: "Direct Interaction with Employers",
+                  body:
+                      "Communicate directly with potential employers, ask questions, and schedule interviews.",
+                  image: _buildImage('employer_interaction_image.jpg'),
+                  decoration: pageDecoration,
+                ),
+                PageViewModel(
+                  title: "Your Privacy Matters",
+                  body:
+                      "We prioritize the security of your data and personal information.",
+                  image: _buildImage('privacy_security_image.jpg'),
+                  decoration: pageDecoration,
+                ),
+                PageViewModel(
+                  title: "Ready to Get Started?",
+                  body:
+                      "Join Kazi Connect and take the next step towards finding employment.",
+                  image: _buildImage('get_started_image.jpg'),
+                  decoration: pageDecoration,
+                ),
+              ],
+              onDone: () => _onIntroEnd(context),
+              onSkip: () => _onIntroEnd(context),
+              // You can override onSkip callback
+              showSkipButton: true,
+              skipOrBackFlex: 0,
+              nextFlex: 0,
+              showBackButton: false,
+              //rtl: true, // Display as right-to-left
+              back: const Icon(Icons.arrow_back),
+              skip: const Text('Skip',
+                  style: TextStyle(fontWeight: FontWeight.w600)),
+              next: const Icon(Icons.arrow_forward),
+              done: const Text('Done',
+                  style: TextStyle(fontWeight: FontWeight.w600)),
+              curve: Curves.fastLinearToSlowEaseIn,
+              controlsMargin: const EdgeInsets.all(16),
+              controlsPadding: kIsWeb
+                  ? const EdgeInsets.all(12.0)
+                  : const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+              dotsDecorator: const DotsDecorator(
+                size: Size(10.0, 10.0),
+                color: Color(0xFFBDBDBD),
+                activeSize: Size(22.0, 10.0),
+                activeShape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                ),
+              ),
+              dotsContainerDecorator: const ShapeDecoration(
+                color: Colors.transparent,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                ),
+              ),
             ),
-            onPressed: () => _onIntroEnd(context),
-          ),
-        ),
-        pages: [
-          PageViewModel(
-            title: "Welcome to Kazi Connect",
-            body:
-                "Connecting You to short-term, full-time jobs and freelancers in Kenya",
-            image: _buildImage('welcome_image.jpg'),
-            decoration: pageDecoration,
-          ),
-          PageViewModel(
-            title: "Discover Job Opportunities",
-            body:
-                "Explore a variety of short-term job listings across different industries and roles.",
-            image: _buildImage('discover_jobs_image.jpg'),
-            decoration: pageDecoration,
-          ),
-          PageViewModel(
-            title: "Easy Application Process",
-            body:
-                "Apply for jobs with a few taps, making your job search efficient and hassle-free.",
-            image: _buildImage('easy_application_image.jpg'),
-            decoration: pageDecoration,
-          ),
-          PageViewModel(
-            title: "Direct Interaction with Employers",
-            body:
-                "Communicate directly with potential employers, ask questions, and schedule interviews.",
-            image: _buildImage('employer_interaction_image.jpg'),
-            decoration: pageDecoration,
-          ),
-          PageViewModel(
-            title: "Your Privacy Matters",
-            body:
-                "We prioritize the security of your data and personal information.",
-            image: _buildImage('privacy_security_image.jpg'),
-            decoration: pageDecoration,
-          ),
-          PageViewModel(
-            title: "Ready to Get Started?",
-            body:
-                "Join Kazi Connect and take the next step towards finding employment.",
-            image: _buildImage('get_started_image.jpg'),
-            decoration: pageDecoration,
-          ),
-        ],
-        onDone: () => _onIntroEnd(context),
-        onSkip: () => _onIntroEnd(context),
-        // You can override onSkip callback
-        showSkipButton: true,
-        skipOrBackFlex: 0,
-        nextFlex: 0,
-        showBackButton: false,
-        //rtl: true, // Display as right-to-left
-        back: const Icon(Icons.arrow_back),
-        skip: const Text('Skip', style: TextStyle(fontWeight: FontWeight.w600)),
-        next: const Icon(Icons.arrow_forward),
-        done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
-        curve: Curves.fastLinearToSlowEaseIn,
-        controlsMargin: const EdgeInsets.all(16),
-        controlsPadding: kIsWeb
-            ? const EdgeInsets.all(12.0)
-            : const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
-        dotsDecorator: const DotsDecorator(
-          size: Size(10.0, 10.0),
-          color: Color(0xFFBDBDBD),
-          activeSize: Size(22.0, 10.0),
-          activeShape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(25.0)),
-          ),
-        ),
-        dotsContainerDecorator: const ShapeDecoration(
-          color: Colors.transparent,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(8.0)),
-          ),
-        ),
-      ),
-    );
+          );
+        });
   }
 }
