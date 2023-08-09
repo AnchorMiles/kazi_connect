ecfe8031146771f60660aee18d34448ed09abd56
samuel-wakoli <swwakoli@gmail.com>
2023-08-09 23:25:49 +0300

    added drawer, and action buttons

--- kazi_connect/lib/presentation/home/home_screen.dart
+++ kazi_connect/lib/presentation/home/home_screen.dart
@@ -62,12 +62,108 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 380),
+              const Divider(),
+              ListTile(
+                onTap: () {},
+                title: const Text("Help & Support"),
+                leading: const Icon(Icons.help_outline),
+              ),
+              ListTile(
+                onTap: () {},
+                title: const Text("Terms of Service"),
+                leading: const Icon(Icons.playlist_add_check_circle_outlined),
+              ),
+              ListTile(
+                onTap: () {},
+                title: const Text("Privacy Policy"),
+                leading: const Icon(Icons.privacy_tip_outlined),
+              ),
+              ListTile(
+                onTap: () {},
+                title: const Text("About"),
+                leading: const Icon(Icons.help_outline),
+              ),
+            ],
+          ),
+        ),
+      ),
       appBar: AppBar(
-        centerTitle: true,
+        // centerTitle: true,
         title: Text(
           appbarTitles[currentPageIndex],
           style: TextStyle(color: Theme.of(context).primaryColor),
         ),
+        actions: [
+          IconButton(onPressed: () {},tooltip: "Search", icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, tooltip: "Post a Job",icon: const Icon(Icons.add)),
+          PopupMenuButton(
+              position: PopupMenuPosition.under,
+              onSelected: (value) {
+                switch (value) {
+                  case "Saved Jobs":
+                    {}
+                  case "Applied Jobs":
+                    {}
+                  case "Invite Friends":
+                    {}
+                  case "Feedback":
+                    {}
+                }
+              },
+              itemBuilder: (ctx) {
+                return const [
+                  PopupMenuItem(
+                      value: "Saved Jobs",
+                      child: Row(
+                        children: [
+                          Icon(Icons.bookmark_border_rounded),
+                          SizedBox(
+                            width: 12,
+                          ),
+                          Text("Saved Jobs")
+                        ],
+                      )),
+                  PopupMenuItem(
+                      value: "Applied Jobs",
+                      child: Row(
+                        children: [
+                          Icon(Icons.view_timeline_outlined),
+                          SizedBox(
+                            width: 12,
+                          ),
+                          Text("Applied Jobs")
+                        ],
+                      )),
+                  PopupMenuItem(
+                      value: "Invite Friends",
+                      child: Row(
+                        children: [
+                          Icon(Icons.share),
+                          SizedBox(
+                            width: 12,
+                          ),
+                          Text("Invite Friends")
+                        ],
+                      )),
+                  PopupMenuItem(
+                      value: "Feedback",
+                      child: Row(
+                        children: [
+                          Icon(Icons.feedback_outlined),
+                          SizedBox(
+                            width: 12,
+                          ),
+                          Text("Feedback")
+                        ],
+                      )),
+                ];
+              })
+        ],
       ),
       body: homeScreenPages[currentPageIndex],
       bottomNavigationBar: _getBottomBar(),
