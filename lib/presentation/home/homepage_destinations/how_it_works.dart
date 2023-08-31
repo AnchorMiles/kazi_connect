45b7eec357b9a1ca5d9bfb68d1d7c8d060fd7ed2
samuel-wakoli <swwakoli@gmail.com>
2023-08-31 12:24:16 +0300

    app design

--- lib/presentation/home/homepage_destinations/how_it_works.dart
+++ lib/presentation/home/homepage_destinations/how_it_works.dart
@@ -14,54 +14,65 @@ class HowItWorks extends StatelessWidget {
         ),
       ),
       body: ListView(
-        children: const [
-          ListTile(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 8, right: 8),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(24),
+              child: Image.asset(
+                "assets/how_it_works.png",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          const ListTile(
             subtitle: Text(
                 "Welcome to Kazi Connect, where finding your hustle is just a few clicks away! "
                 "We've designed our platform to make the job search process easy, efficient, "
                 "and tailored to your preferences. Here's how Kazi Connect works"),
           ),
-          ListTile(
+          const ListTile(
             title: Text("Explore Job Categories"),
             subtitle: Text(
                 "Begin by exploring a wide range of job categories tailored to your "
                 "interests and skills. Whether you're looking for full-time positions, "
                 "part-time gigs, attachments, internships, or short-term projects, we've got you covered."),
           ),
-          ListTile(
+          const ListTile(
             title: Text("View Job Listings"),
             subtitle: Text(
                 "Browse through detailed job listings that include all the essential "
                 "information you need to make an informed decision. You'll find job "
                 "descriptions, requirements, company details, and more."),
           ),
-          ListTile(
+          const ListTile(
             title: Text("Save Jobs"),
             subtitle: Text(
                 "Found a job that caught your eye? Save it to your favorites "
                 "list for easy access later."),
           ),
-          ListTile(
+          const ListTile(
             title: Text("Apply with Ease"),
             subtitle: Text(
                 "When you're ready to apply, we've streamlined the process. "
                 "Submit your application directly through the app, and keep track of "
                 "your applications in one convenient place."),
           ),
-          ListTile(
+          const ListTile(
             title: Text("Connect with Employers"),
             subtitle: Text(
                 "Experience the power of seamless communication. Chat directly "
                 "with employers, ask questions, and showcase your skills "
                 "to stand out from the crowd."),
           ),
-          ListTile(
+          const ListTile(
             title: Text("Stay Informed"),
             subtitle: Text(
                 "Stay up-to-date with the latest job market trends, industry insights, "
                 "and tips for acing interviews through our regularly updated resources."),
           ),
-          ListTile(
+          const ListTile(
             title: Text("User-Friendly Experience"),
             subtitle: Text(
                 "We've designed Kazi Connect to be user-friendly and intuitive. "
