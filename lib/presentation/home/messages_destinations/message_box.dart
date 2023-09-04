30cb75b4ee32416baca28b27f4abd66a50ac5d00
samuel-wakoli <swwakoli@gmail.com>
2023-09-04 23:27:49 +0300

    design for the chat page

--- lib/presentation/home/messages_destinations/message_box.dart
+++ lib/presentation/home/messages_destinations/message_box.dart
@@ -8,17 +8,67 @@ class MessageBoxPage extends StatefulWidget {
 }
 
 class _MessageBoxPageState extends State<MessageBoxPage> {
+  // TODO: Initialize Firebase instance and reference here
+
+  TextEditingController _messageController = TextEditingController();
+  List<Map<String, dynamic>> _messages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // TODO: Initialize Firebase reference for reading messages and listen for updates
+  }
+
+  // Function to send a message to Firebase
+  void _sendMessage(String messageText) {
+    // TODO: Send message to Firebase here
+    _messageController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title: const Text("Sender Name"),
-        actions: const [
-          Icon(
-            Icons.account_circle_outlined,
-            size: 38,
-          )
+        title: Text('Sender Name'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                final message = _messages[index];
+                return ListTile(
+                  title: Text(message['text']),
+                  subtitle: Text(message['sender']),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _messageController,
+                    decoration: InputDecoration(
+                      hintText: 'Type a message...',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () {
+                    final messageText = _messageController.text;
+                    if (messageText.isNotEmpty) {
+                      _sendMessage(messageText);
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
